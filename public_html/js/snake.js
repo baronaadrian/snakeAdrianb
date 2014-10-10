483169488637df7b47523d63a58469b10d548693
baronaadrian <barona.adrian@yahoo.com>
2014-10-10 14:03:10 -0700

    snake adrian game

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,17 +1,42 @@
 var snake;
+var snakeLength;
+var snakeSize;  
 
 var context;
 var screenWidth;
 var screenHeight;
 
+gameInitialize();
+gameDraw();
+
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
-}
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+    }
 
 function gameLoop() {
     
 }
 
 function gameDraw() {
+    context.fillStyle = "rgb(12, 247, 130)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
     
 }
