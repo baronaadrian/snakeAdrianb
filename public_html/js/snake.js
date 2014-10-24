2ad5b2e7ecf4e53a5b9a75c769f5a4ad78741f26
baronaadrian <barona.adrian@yahoo.com>
2014-10-24 14:04:07 -0700

    my anaconda dont

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,13 +1,22 @@
+/* --------------------------------------------------
+ * Variables
+ * --------------------------------------------------
+ */
+
 var snake;
 var snakeLength;
 var snakeSize;  
+
+var food;
 
 var context;
 var screenWidth;
 var screenHeight;
 
-gameInitialize();
-gameDraw();
+gameInitialize();snakeInitialize();
+
+foodInitialize();
+setInterval(gameLoop, 2000/30);
 
 function gameInitialize() {
     var canvas = document.getElementById("game-screen");
@@ -21,37 +30,57 @@ function gameInitialize() {
     }
 
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
 }
 
 function gameDraw() {
     context.fillStyle = "rgb(12, 247, 130)";
     context.fillRect(0, 0, screenWidth, screenHeight);
-}
-
+    }
+  
 function snakeInitialize() {
   snake = [];
   snakeLength = 5;
-  snakeSize = 20;
+  snakeSize = 21;
 
-  for(var index = 0; index < snakeLength; index++) {
+  for(var index = snakeLength - 1; index >= 0; index--) {
                 snake.push({
                   x: index,
                   y: 0
-               
               });
          }
    }
-   
    
   function snakeDraw() {
    for(var index =0; index < snake.length; index++) {
        context.fillStyle = "white";
        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
-   } 
-        
+   }                                   
 }
 
 function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
     
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
+
+function foodInitialize() {
+    food = {
+        x: 0,
+        y: 0
+    };
+}
+
+function foodDraw() {
+    context.fillStyle = "white";
+    context.fillRect(food.x, food.y, snakeSize, snakeSize);
 }
